f9f9bebaf75417c1c5a03af3005e29371baf6be1
AhsanFarazkhan <136695299+AhsanFarazkhan@users.noreply.github.com>
2024-12-03 10:56:57 +0500

    Add files via upload

---
+++ lab mvc i/JavaApplication3/src/controller/Controller.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import model.Model;
+import view.View;
+
+public class Controller {
+
+    private Model model;
+    private View view;
+
+    public Controller(Model model, View view) {
+        this.model = model;
+        this.view = view;
+    }
+
+    public void setData(String data) {
+        model.setData(data);
+    }
+
+    public String getData() {
+        return model.getData();
+    }
+
+    public void updateView() {
+        view.printData(model.getData());
+    }
+}
